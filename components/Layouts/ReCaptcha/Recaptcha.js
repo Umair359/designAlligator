ff0e30d4e7073a08b6ba48778cfa08b2c20a4048
Umair359 <umairulislamdh359@gmail.com>
2024-02-05 19:38:49 +0500

    d

--- components/Layouts/ReCaptcha/Recaptcha.js
+++ components/Layouts/ReCaptcha/Recaptcha.js
@@ -1,11 +1,12 @@
 import React, { useRef } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 
-const TEST_SITE_KEY = "6LdgjL0kAAAAADdI4URPhrZnJm5ibmyYRc-qh8gw";
-const Recaptcha = React.forwardRef(({setRecaptcha},ref) => {
+// const TEST_SITE_KEY = "6LdgjL0kAAAAADdI4URPhrZnJm5ibmyYRc-qh8gw";
+const TEST_SITE_KEY = "6Le2pGcpAAAAAEEDSJU2omY83QfVmQoWkp_T9FCI";
+const Recaptcha = React.forwardRef(({ setRecaptcha }, ref) => {
   const handleChange = (value) => {
     setRecaptcha(value)
-    
+
   };
   return (
     <div
